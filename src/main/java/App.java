cb1dd166c9a195eea0d65f4173dbdec07e4f0212
Guyoguyes <g.abuba43@gmail.com>
2020-06-04 15:17:08 +0300

    heroku port changed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,29 +14,15 @@ import static spark.Spark.*;
 
 
 public class App {
-//    static int getHerokuAssignedPort() {
-//        ProcessBuilder processBuilder = new ProcessBuilder();
-//        if (processBuilder.environment().get("PORT") != null) {
-//            return Integer.parseInt(processBuilder.environment().get("PORT"));
-//        }
-//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-//    }
-    public static void main(String[] args) {
-        ProcessBuilder process = new ProcessBuilder();
-        Integer port;
-
-        // This tells our app that if Heroku sets a port for us, we need to use that port.
-        // Otherwise, if they do not, continue using port 4567.
-
-        if (process.environment().get("PORT") != null) {
-            port = Integer.parseInt(process.environment().get("PORT"));
-        } else {
-            port = 4567;
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-
-        port(port);
-
-
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+    public static void main(String[] args) {
+       port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
 
